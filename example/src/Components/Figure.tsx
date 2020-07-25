c2d12866e50849e732393feb14fd759128574175
David Baumgartner <dv@bmgr.me>
2020-07-25 18:26:09 +0200

    📝 adapting demo to accomodate xsl:choose

---
+++ example/src/Components/Figure.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import ctx from "./context";
+import Transformer from "react-xml-transformer";
+import ReactDOMServer from "react-dom/server";
+
+export default class Para extends Transformer.Template {
+  render() {
+    const alt = (
+      <Transformer.Choose
+        {...this.ctx}
+        cases={[
+          {
+            test: "./p:img/@alt",
+            node: <Transformer.ValueOf {...this.ctx} select="./p:img/@alt" />,
+          },
+          {
+            test: "./p:title",
+            node: (
+              <Transformer.ValueOf {...this.ctx} select="./p:title/text()" />
+            ),
+          },
+        ]}
+        otherwise={"no alternating text"}
+      />
+    );
+    return (
+      <figure>
+        <img
+          src={ReactDOMServer.renderToString(
+            <Transformer.ValueOf {...this.ctx} select="./p:img/@src" />
+          )}
+          alt={ReactDOMServer.renderToString(alt)}
+        />
+        <h2>
+          <Transformer.ApplyTemplates {...this.ctx} select="./p:title/text()" />
+        </h2>
+      </figure>
+    );
+  }
+}
+
+ctx.register(Para, { path: "p:figure" });
--- example/src/Components/index.ts
+++ example/src/Components/index.ts
@@ -2,6 +2,7 @@ import "./Root";
 import "./Title";
 import "./Text";
 import "./Para";
+import "./Figure";
 import ctx from "./context";
 
 export default ctx;
